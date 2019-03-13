af75982a008781288753800c08ac4bfbfda09210
Michael Upton <uptonm@wit.edu>
2019-03-13 12:13:09 -0400

    updated server to use logger service on connect

--- server/app.js
+++ server/app.js
@@ -1,15 +1,33 @@
 const express = require('express');
 const mongoose = require('mongoose');
 const bodyParser = require('body-parser');
+const passport = require('passport');
+const morgan = require('morgan');
 const cors = require('cors');
 require('dotenv').config();
+
+require('./models/user');
+require('./services/jwtAuth');
 
 const app = express();
 const router = express.Router();
 
-app.use('/api', router);
-app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.use(morgan('dev'));
 app.use(cors());
+
+app.use('/api', require('./routes/auth.routes'));
+app.use(
+  '/user',
+  passport.authenticate('jwt', { session: false }),
+  require('./routes/secure.routes')
+);
+
+//Handle errors
+app.use((err, req, res, next) => {
+  res.status(err.status || 500);
+  res.json({ error: err });
+});
 
 module.exports = app;
