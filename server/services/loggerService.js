65792bd72e8ac8f055516b71d9d444de979d0022
Michael Upton <uptonm@wit.edu>
2019-03-19 08:01:15 -0400

    quick logger refactor

--- server/services/loggerService.js
+++ server/services/loggerService.js
@@ -1,11 +1,9 @@
 const colors = require('colors');
-module.exports = {
-  msg: msg => {
-    let d = new Date().toLocaleTimeString();
-    return console.log(`${colors.grey(d)} ✨ ${colors.green(msg)}`);
-  },
-  err: msg => {
-    let d = new Date().toLocaleTimeString();
-    return console.log(`${colors.grey(d)} ❗️ ${colors.red(msg)}`);
-  }
+exports.msg = msg => {
+  let d = new Date().toLocaleTimeString();
+  return console.log(`${colors.grey(d)} ✨ ${colors.green(msg)}`);
+};
+exports.err = msg => {
+  let d = new Date().toLocaleTimeString();
+  return console.log(`${colors.grey(d)} ❗️ ${colors.red(msg)}`);
 };
