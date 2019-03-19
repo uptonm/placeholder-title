cd3e23fa25532f8be696486a5f8cfabd3761dafb
Michael Upton <uptonm@wit.edu>
2019-03-19 08:10:58 -0400

    fixing logger

--- server/services/loggerService.js
+++ server/services/loggerService.js
@@ -1,5 +1,4 @@
 const colors = require('colors');
-<<<<<<< HEAD
 exports.msg = msg => {
   let d = new Date().toLocaleTimeString();
   return console.log(`${colors.grey(d)} ✨ ${colors.green(msg)}`);
@@ -7,15 +6,4 @@ exports.msg = msg => {
 exports.err = msg => {
   let d = new Date().toLocaleTimeString();
   return console.log(`${colors.grey(d)} ❗️ ${colors.red(msg)}`);
-=======
-module.exports = {
-  msg: msg => {
-    let d = new Date().toLocaleTimeString();
-    return console.log(`${colors.grey(d)} ✨  ${colors.green(msg)}`);
-  },
-  err: msg => {
-    let d = new Date().toLocaleTimeString();
-    return console.log(`${colors.grey(d)} ❗️  ${colors.red(msg)}`);
-  }
->>>>>>> 678fb86e1605e525502bfe5dff9917a9008a2c28
 };
