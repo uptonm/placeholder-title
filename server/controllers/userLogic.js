15a5dd9ee54863cda85e378cc536fb5728d4a27c
Michael Upton <uptonm@wit.edu>
2019-04-03 10:10:42 -0400

    Added route and controller to follow user

--- server/controllers/userLogic.js
+++ server/controllers/userLogic.js
@@ -17,7 +17,7 @@ exports.getUser = async (req, res) => {
 exports.putUser = async (req, res) => {
   const exists = await user.findOne({ email: req.user.email });
   if (exists) {
-    await user.findByIdAndUpdate(exists._id, req.body, (error) => {
+    await user.findByIdAndUpdate(exists._id, req.body, error => {
       if (error) return res.send(error);
       return res.send({
         user: exists._id,
@@ -34,9 +34,9 @@ exports.putUser = async (req, res) => {
   }
 };
 exports.deleteUser = async (req, res) => {
-  const exists = await user.findOne({email: req.user.email});
+  const exists = await user.findOne({ email: req.user.email });
   if (exists) {
-    await user.findByIdAndDelete(exists._id, (error) => {
+    await user.findByIdAndDelete(exists._id, error => {
       if (error) throw error;
       return res.send({
         user: exists._id,
@@ -59,7 +59,6 @@ exports.getFollowers = async (req, res) => {
     });
   }
 };
-
 exports.getFollowing = async (req, res) => {
   const exists = await user.findById(req.user._id).populate('following');
   if (exists) {
@@ -67,9 +66,65 @@ exports.getFollowing = async (req, res) => {
   } else {
     res.status(404).send({
       error: {
+        status: 404,
         // eslint-disable-next-line quotes
         message: "Couldn't find your profile. Try again later."
       }
     });
   }
+};
+exports.followUser = async (req, res) => {
+  // Check if this user exists
+  let userExists = await user.findById(req.user._id);
+  if (!userExists) {
+    return res.status(404).send({
+      error: {
+        status: 404,
+        // eslint-disable-next-line quotes
+        message: "Couldn't find your profile. Try again later."
+      }
+    });
+  }
+  // Confirm that request contains id field
+  if (!req.body.id || req.body.length <= 0) {
+    return res.status(400).send({
+      error: {
+        status: 400,
+        message: 'This route requires an id field'
+      }
+    });
+  }
+  // Check if this user is already following
+  if (userExists.following.length > 0 && !userExists.following.includes(req.body.id)) {
+    return res.status(400).send({
+      error: {
+        status: 400,
+        message: 'You are already following this user'
+      }
+    });
+  }
+  // Check if user to follow exists
+  let userToFollow = await user.findById(req.body.id);
+  if (!userToFollow) {
+    return res.status(404).send({
+      error: {
+        status: 404,
+        // eslint-disable-next-line quotes
+        message: "Couldn't find your profile. Try again later."
+      }
+    });
+  }
+  // Add this user to user to follow's followers
+  userToFollow.followers.push(req.user._id);
+  await userToFollow.save();
+  // Add user to follow to this user's following
+  userExists.following.push(req.body.id);
+  await userExists.save();
+
+  res.status(200).send({
+    success: {
+      status: 200,
+      message: `User ${req.body.id} followed`
+    }
+  });
 };
