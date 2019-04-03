e71afc4f735dde84df189890253d7687cfc8fed4
Michael Upton <uptonm@wit.edu>
2019-04-03 09:32:58 -0400

    Changed user route to /user to ease confusion

--- server/routes/user.routes.js
+++ server/routes/user.routes.js
@@ -5,8 +5,8 @@ const router = express.Router();
 //Lets say the route below is very sensitive and we want only authorized users to have access
 
 //Displays information tailored according to the logged in user
-router.get('/users', actions.getUser);
-router.put('/users', actions.putUser);
-router.delete('/users', actions.deleteUser);
+router.get('/user', actions.getUser);
+router.put('/user', actions.putUser);
+router.delete('/user', actions.deleteUser);
 
 module.exports = router;
